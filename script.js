6d3e83a7ae06e03f15676039737905407a2f346f
Jamille Rooks <jamillerooks@outlook.com>
2022-02-08 22:19:47 -0500

    The current weather is populating the dashbord

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var cityHistoryEl = document.getElementById('city-search-history');
 var searchBtn = document.getElementById('searchBtn');
 var citySearchHistoryEl = document.querySelector('#searchesHistory');
 var currentPictureEl = document.querySelector('#current-picture');
-//var weatherForecastEl = `https://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+//var weatherForecastEl = `https://openweathermap.org/img/w/${data.current.weather[0].icon}.png`;
 var currentTempEl = document.querySelector('#current-temp');
 var currentWindEl = document.querySelector('#current-wind');
 var currentHumidityEl = document.querySelector('#current-humidity');
@@ -24,20 +24,76 @@ function searchCoordinates(city) {
 	)
 		.then((response) => response.json())
 		.then((data) => {
-			console.log(data);
-			getWeather(data.coord.lat, data.coord.lon);
+		
+			getWeather(data.coord.lat, data.coord.lon, city);
 		});
 }
 
-function getWeather(lat, lon) {
+
+function getWeather(lat, lon, city) {
 	var oneCallURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
 	fetch(oneCallURL)
 		.then((response) => response.json())
-		.then((oneCallData) => {
-			console.log(oneCallData);
+		.then((data) => {
+			console.log(data);
+      let date = document.querySelector('#date');
+      var current = $.now();
+      var maxDate = new Date(current);
+      var currentDate = maxDate.toString();
+      date.innerHTML = currentDate;
+      var weatherIcon = data.current.weather[0].icon;
+      //weatherIcon.setAttribute('src', weatherIcon);
+      weatherIcon = ('src', `https://openweathermap.org/img/w/${weatherIcon}.png`);
+      var temp = document.getElementById('current-temp');
+      temp.innerHTML = `<span> Temp </span>${Math.round(
+        data.current.temp
+      )}<span>℉</span>`;
+      var wind = document.getElementById('current-wind');
+      wind.innerHTML = `<span> Wind </span>${Math.round(
+        data.current.wind_speed
+      )}<span>mph</span>`;
+      var humidity = document.getElementById('current-humidity');
+      humidity.innerHTML = `<span> Humidity </span>${data.current.humidity}<span>%</span>`;
+      cityName = document.getElementById('cityName');
+      cityName.innerHTML = city;
+
+      var uv = document.getElementById('uvIndex');
+      uv.innerHTML = data.current.uvi;
+      var uvColor = document.getElementById('uv-color');
+      uvColor.innerHTML = `UV Index: ${uv}`;
+
+      if (uv < 3) {
+        uvColor.setAttribute('class', 'uvcard green');
+      } else if (uv > 6) {
+        uvColor.setAttribute('class', 'uvcard yellow');
+      } else if (uv < 8) {
+        uvColor.setAttribute('class', 'uvcard orange');
+      } else {
+        uvColor.setAttribute('class', 'uvcard red');
+      }
+
+      for (var i = 0; i < 6; i++) {
+        var day = moment.unx(data.daily[i].dt).format('MM/dd/yyyy');
+        var dayTemp = data.daily[i].temp.day;
+        var dayWind = data.daily[i].wind_speed;
+        var dayHumidity = data.daily[i].humidity;
+        var dayIcon = data.daily[i].weather[0].icon;
+        dayIcon.setAttribute(
+          (src = `https://openweathermap.org/img/w/${dayIcon}.png`)
+        );
+      }
+
 			buildHistory();
 		});
+	
+	// console.log(city);
+	//console.log(date);
+	// console.log(weather.temp);
+	// console.log(weather.wind_speed);
+	// console.log(weather.humidity);
+	// console.log(weather.uvi);
 }
+
 
 function buildHistory() {
 	cityHistoryEl.innerHTML = '';
@@ -56,6 +112,7 @@ function buildHistory() {
 		newBtn.addEventListener('click', function () {
 			searchCoordinates(cityHistory[i]);
 		});
+
 		cityHistoryEl.append(newBtn);
 	}
 }
@@ -70,55 +127,18 @@ function saveToLocal() {
 	localStorage.setItem('cityHistory', JSON.stringify(searches));
 }
 
-searchBtn.addEventListener('click', function () {
+searchBtn.addEventListener('click', function (event) {
+	event.preventDefault();
 	var cityToSearch = cityInputEl.value;
 	searchCoordinates(cityToSearch);
 	saveToLocal();
 });
+
 buildHistory();
-currentWeather();
 
-function currentWeather(city, weather, time) {
-	let date = document.querySelector('#date');
-	var current = $.now();
-	var maxDate = new Date(current);
-	var currentDate = maxDate.toString();
-	date.innerHTML = currentDate;
-	var weatherIcon = data.weather[0].icon;
-	weatherIcon.setAttribute =
-		('src', `https://openweathermap.org/img/w/${weatherIcon}.png`);
-	var temp = document.getElementById('current-temp');
-	temp.innerHTML = `<span> Temp </span>${Math.round(
-		data.coord.weather.main.temp
-	)}<span>℉</span>`;
-	var wind = document.getElementById('current-wind');
-	wind.innerHTML = `<span> Wind </span>${Math.round(
-	data.coord.weather.wind.speed
-	)}<span>mph</span>`;
-	var humidity = document.getElementById('current-humidity');
-	humidity.innerHTML = `<span> Humidity </span>${data.coord.weather.main.humidity}<span>%</span>`;
-	var uv = document.getElementById('uvIndex');
-	var uvColor = document.getElementById('uv-color');
-	cityName = document.getElementById('cityName');
-	cityName.innerHTML = city;
-	todayIcon.setAttribute('src', weatherIcon);
+// $('.newBtn').on('click', function (event) {
+// 	event.preventDefault();
+// 	city = $(this).text();
+// 	searchCoordinates(city);
+// });
 
-	uv.innerHTML = data.daily.uvi.uvi;
-
-	if (data.daily.uvi < 3) {
-		uvcolor.setAttribute('class', 'uvcard green');
-	} else if (data.daily.uvi < 6) {
-		uvColor.setAttribute('class', 'uvcard yellow');
-	} else if (data.daily.uvi < 8) {
-		uvColor.setAttribute('class', 'uvcard orange');
-	} else {
-		uvColor.setAttribute('class', 'uvcard red');
-	}
-
-	console.log(city);
-	console.log(date);
-	console.log(weather.temp);
-	console.log(weather.wind_speed);
-	console.log(weather.humidity);
-	console.log(weather.uvi);
-}
